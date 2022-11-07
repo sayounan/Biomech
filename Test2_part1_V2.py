8e70fff8412906e9d2a72d73a406e2364a571896
Juan Orduna <116674297+IHateProgramming2022@users.noreply.github.com>
2022-11-06 19:16:30 -0700

    Reverted Sari's changes, got the elements and nodes for all files

--- Test2_part1_V2.py
+++ Test2_part1_V2.py
@@ -6,7 +6,7 @@ Created on Tue Oct 25 14:07:18 2022
 """
 
 # Change this line for your computer
-locPath = r"/Users/sayounan/Documents/University/2022 2-Fall/Biomechanics Intro (BIOE 3020)/Coding Assignment1"
+locPath = r"C:\Users\hepbo\OneDrive\Documents\New UCA Fall 2022\Python Fall 2022\Test 2 part 1"
 
 
 def read(local_path):  # This is for reading data
@@ -14,12 +14,13 @@ def read(local_path):  # This is for reading data
     import os
     import numpy as np
 
-    filenames = os.listdir(local_path)
-    filepaths = [os.path.join(local_path, name) for name in filenames]
+    folderpath = local_path
+    filenames = os.listdir(folderpath)
+    filepaths = [os.path.join(folderpath, name) for name in filenames]
 
     d = {}
-    for path in filepaths:
-        for i in range(len(filenames)):
+    for path in filepaths:                  
+        for i in range(len(filenames)):     
             if ".ucd" in filenames[i]:
                 if "Basic" in filenames[i]:
                     d[f'{filenames[i]} Nodes'] = np.genfromtxt(filenames[i], skip_footer=7, skip_header=1,
@@ -31,9 +32,7 @@ def read(local_path):  # This is for reading data
                     d[f'{filenames[i]} Elems'] = np.genfromtxt(filenames[i], skip_header=405, delimiter=' ')
             else:
                 continue
-
-    dict = sorted(d.keys())
-    return dict
+    return d
 
 
 dataIn = read(locPath)
@@ -50,15 +49,19 @@ To "index" the dictionary use list(dataIn.values())[#], also the order of
 indexing goes [item in dictionary][row][column]
 """
 
-# Loop to get the element values from all the element files
+#Loop to get all the nodes from all node files 
 AllNodes = []
 AllElems = []
-
+    
 for i in range(0, len(dataIn.values()), 2):
     for j in range(len(list(dataIn.values())[i])):
-        AllNodes.append(list(dataIn.values())[i][j][3:6])
-
-# Loop to get the node values from all the node files
+        AllNodes.append(list(dataIn.values())[i][j][1:4])
+    
+#Loop to get all the elements from all the element file
 for i in range(1, len(dataIn.values()), 2):
     for j in range(len(list(dataIn.values())[i])):
-        AllElems.append(list(dataIn.values())[i][j][1:4])
+        AllElems.append(list(dataIn.values())[i][j][3:6])
+    
+    
+    
+    
